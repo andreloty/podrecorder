7afcc665ce5318ebe301c1544f883887cbe01bcb
André Lima <andre.lima@hbsis.com.br>
2020-04-22 01:05:21 -0300

    Melhorias no teste de integração

--- backend/__tests__/integration/authorization.test.js
+++ backend/__tests__/integration/authorization.test.js
@@ -7,7 +7,7 @@ beforeEach(async () => {
 })
 
 describe('Authorization (signup and login)', () => {
-  it('should create a new user profile for authentication', async () => {
+  it('should create a new user profile for authentication and receive a token', async () => {
     const response = await request(app).post('/api/v1/signup').send({
       firstName: 'Andre',
       lastName: 'Lima',
@@ -26,7 +26,7 @@ describe('Authorization (signup and login)', () => {
       password: '123456'
     })
 
-    expect(response.status).toBe(201)
+    expect(response.status).toBe(200)
     expect(response.body.id).toBeGreaterThan(0)
     expect(response.body.token).toBeDefined()
   })
