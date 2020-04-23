c4638b3d01c1cadcdf917f226066117bc152b0b2
André Lima <andre.lima@hbsis.com.br>
2020-04-22 23:28:19 -0300

    Correção da configuação do socket.io

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,12 +8,12 @@ const indexRouter = require('./routes/index')
 
 const app = express()
 
-app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(cors())
 
 app.use('/', indexRouter)
 
--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Typography from '@material-ui/core/Typography'
 import Link from '@material-ui/core/Link'
 import { useHistory } from 'react-router-dom'
+import io from 'socket.io-client'
+const SOCKET_IO_URL = 'http://localhost:3333'
+const socket = io(SOCKET_IO_URL)
 
 export default function Home() {
   const history = useHistory()
+  const [connected, setConnected] = useState('desconectado')
+
+  socket.on('connect', () => {
+    setConnected('conectado')
+  })
 
   const login = (e) => {
     e.preventDefault()
@@ -14,7 +22,10 @@ export default function Home() {
   return (
     <div>
       <Typography component="h1" variant="h5">
-        Olá Mundo
+        Olá Mundo!
+      </Typography>
+      <Typography component="h3" variant="h5">
+        Você está {connected}.
       </Typography>
       <Link href="#" onClick={login} variant="body2">
         Login
